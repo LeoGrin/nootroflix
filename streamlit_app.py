1a10530b0aad73b2dfb8a61d33c1ab60e44301b9
LeoGrin <leo.grinsztajn@polytechnique.edu>
2022-01-19 13:51:33 +0100

    add garbage collector

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import time
 from train_model import predict, evaluate
 from utils import save_new_ratings, generate_user_id, load_collection, save_position
 import pandas as pd
+import gc
 
 
 
@@ -311,3 +312,4 @@ if st.session_state["mode"] == "results":
     st.write("Our algorithm matches you to people with similar ratings, and tells you other nootropics they liked.")
     st.write("The initial data comes from the 2016 SlateStarCodex Nootropics survey results.")
     st.write("Some of the question are inspired by the 2016 and 2020 SlateStarCodex nootropics surveys.")
+    gc.collect()
