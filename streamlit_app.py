e7f6e156a9e67ad69f2dee7bce2eb155407e75e2
LeoGrin <leo.grinsztajn@polytechnique.edu>
2022-01-20 15:16:57 +0100

    only prediction

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,212 +117,48 @@ def deploy():
     st.set_page_config(page_title="️Nootroflix", page_icon=":brain:", layout="centered", initial_sidebar_state="auto",
                        menu_items=None)
 
-    nootropics_dic, all_nootropics = get_metadata('data/nootropics_metadata.csv')
 
-    user_id = 2
+    rating_example = {'Modafinil': 6,
+                      'Caffeine': 6,
+                      'Theanine': 7,
+                      'Creatine': 4,
+                      'Melatonin': 0,
+                      'LSD (microdose)': 0,
+                      'Adderall': 8,
+                      "Phenibut": 6,
+                      "Nicotine": 7}
 
-    if "already_run" not in st.session_state.keys():
-        st.session_state.already_run = True
-    else:
-        if not user_id:
-            print("no")
-            # print("No username found, generating one...")
-            # TODO simpler with session state?
-            # user_id = generate_user_id("data/dataset_clean_right_names.csv", session_id) #cached, refreshed if new session_id
-            # print("UserID: {}".format(user_id))
-            # print("cookie set")
-        if "save_start" not in st.session_state.keys():  # now that we have a user_id, we can save the start time
-            st.session_state.save_start = True
-            st.session_state.scroll = False  # weird hack to allow scrolling to the top on refresh
 
     st.title(':brain: Nootroflix')
     original_title = '<p style="color:Pink; font-size: 20px;">Rate the nootropics you\'ve tried, and we\'ll tell you which one should work for you!</p>'
     st.markdown(original_title, unsafe_allow_html=True)
 
-    if "mode" not in st.session_state.keys():
-        st.session_state["mode"] = "selection"
+    st.header("🧠 Your results")
+    st.write("Our model predicted these ratings for you:")
+    st.caption("Some nootropics don't have enough data right now to be included.")
+    st.caption(
+        """ ⚠️"Nootropic" is used here in a broad sense, and some of these substances present a risk of side effects or addiction.""")
+    with st.spinner('Loading...'):
+        new_result_df = predict(rating_example)
+        new_result_df = new_result_df.sort_values("Prediction", ascending=False, ignore_index=True)
 
-    if st.session_state["mode"] == "selection":
-        st.header("How do I use it?")
-        st.markdown(
-            """ **First tell us which nootropics you have tried, then rate your subjective experience on a scale of 0 to 10.**""")
-        st.markdown("""It should take less than 5 minutes and you won't need to create an account!""")
-        st.write("")
-        select_form = st.form("select-form")
-        with select_form:
-            st.header("What have you tried?")
-            st.form_submit_button("Reset 🗑", on_click=reset_selection)
-            n_cols = 2  # TODO align columns ?
-            for type in nootropics_dic.keys():
-                st.header("🧠 {}".format(type))
-                cols = st.columns(n_cols)
-                for i, nootropic in enumerate(nootropics_dic[type]):
-                    with cols[i % n_cols]:
-                        retrieve_widget_value("checkbox_{}_{}".format(type, nootropic))
-                        st.checkbox("{}".format(nootropic), key="checkbox_{}_{}".format(type, nootropic))
-                st.write("")
-            retrieve_widget_value("select_box_selection")
-            st.selectbox("Search for a nootropic you might have missed", [""] + list(all_nootropics),
-                         key="select_box_selection")
-            st.write("")
-            st.form_submit_button("Next", on_click=lambda: go_to_mode_rating(all_nootropics))
+        new_result_df = new_result_df.merge(pd.read_csv("data/nootropics_metadata.csv", sep=";"), on="nootropic",
+                                            how="left")
+        new_result_df["Prediction"] = new_result_df["Prediction"].apply(lambda x: round(x, 1))
+        new_result_df["Mean rating"] = new_result_df["Mean rating"].apply(lambda x: round(x, 1))
+        # new_result_df["Boost"] = new_result_df["Boost"].apply(lambda x: round(x, 1))
 
-    if st.session_state["mode"] == "rating":
-        st.subheader("Please rate the net benefit of each nootropic for you:")
-        st.markdown("""    \n 🧠 0 means a substance was totally useless, or had so many side effects you couldn't continue taking it.
-        \n 🧠 1 - 4 means subtle effects, maybe placebo but still useful.
-        \n 🧠 5 - 9 means strong effects, definitely not placebo.
-        \n 🧠 10 means life - changing.""")
-        st.write("")
-        st.caption("For lifestyle interventions like diets, please only rate direct cogntive improvement.")
-        st.write("")
+        styled_names = []
+        for i, row in new_result_df.iterrows():
+            styled_names.append(make_name(row))
+        new_result_df["Nootropic"] = styled_names
+        new_result_df = new_result_df[["Nootropic", "Prediction", "Mean rating"]]
+        # new_result_df.columns = ["Nootropic",
+        #                         """<div title="The rating we predict you would enter if you tried the nootropic">Prediction</div>""",
+        #                         """<div title="The mean of other users ratings">Mean rating</div>"""]
+        st.write(new_result_df.to_html(escape=False, index=False),
+                 unsafe_allow_html=True)  # .style.format("{:.1f}").applymap(left_align)
 
-        possible_issues_list = ["None",
-                                "Developed tolerance",
-                                "Developed addiction",
-                                "Had to stop because of side effects",
-                                "Persistent side effects",
-                                "Other issues"]
-
-        rating_form = st.form("rating-form")
-        with rating_form:
-            cols = st.columns(2)
-            i = 0
-            for nootropic in all_nootropics:
-                if st.session_state["permanent_checkbox_{}".format(nootropic)] or st.session_state["permanent_select_box_selection"] == nootropic:
-                    with cols[i % 2]:
-                        i += 1
-                        retrieve_widget_value("slider_{}".format(nootropic))
-                        st.slider("{} rating".format(nootropic), min_value=0, max_value=10,
-                                  key="slider_{}".format(nootropic))
-                        retrieve_widget_value("radio_{}".format(nootropic))
-                        st.multiselect("Issues with {}".format(nootropic), possible_issues_list,
-                                       key="radio_{}".format(nootropic))
-                        # for issue in possible_issues_list:
-                        #    retrieve_widget_value("radio_{}_{}".format(nootropic, issue))
-                        #    st.checkbox(issue, key="radio_{}_{}".format(nootropic, issue))
-                        st.write("")
-                        st.write("")
-            st.write("")
-            col1, col2 = st.columns([1, 9])
-            with col1:
-                st.form_submit_button("Back", on_click=go_to_mode("selection"))  # TODO save results when going back
-            with col2:
-                st.form_submit_button("Next", on_click=go_to_mode("questions"))
-
-    if st.session_state["mode"] == "questions":
-        question_form = st.form("question-form")
-        with question_form:
-            # st.form_submit_button("Back", on_click=go_to_mode("ratings"))
-            st.header("🧠 A few questions")
-            retrieve_widget_value("question_gender")
-            st.selectbox("Gender", ["-", "Male", "Female", "Other"], key="question_gender")
-            retrieve_widget_value("question_age")
-            st.number_input("Age", max_value=100, key="question_age")
-            options = ["Not at all a reason", "Yes, a minor reason", "Yes, a major reason"]
-            retrieve_widget_value("question_anxiety")
-            st.radio("Do / Did you take nootropics to help with anxiety?", options=options, key="question_anxiety")
-            retrieve_widget_value("question_focus")
-            st.radio("Do / Did you take nootropics to help with focus?", options=options, key="question_focus")
-            retrieve_widget_value("question_mood")
-            st.radio("Do / Did you take nootropics to help with mood?", options=options, key="question_mood")
-            retrieve_widget_value("question_cognition")
-            st.radio("Do / Did you take nootropics to help with cognition / memory?", options=options,
-                     key="question_cognition")
-            retrieve_widget_value("question_motivation")
-            st.radio("Do / Did you take nootropics to help with motivation / energy?", options=options,
-                     key="question_motivation")
-            retrieve_widget_value("question_libido")
-            st.radio("Do / Did you take nootropics to help with libido or other sex-related reasons?", options=options,
-                     key="question_libido")
-            retrieve_widget_value("question_other")
-            st.text_input("Another reason you take / took nootropics?", key="question_other")
-            st.write("")
-            st.write("")
-            retrieve_widget_value("favorite_noot")
-            favorite_noot = st.text_input("What are your favorite nootropics not mentioned here?", key="favorite_noot")
-            st.text("")
-            retrieve_widget_value("not_true_ratings")
-            st.checkbox(
-                "Check this box if you're not entering your true ratings / infos (prevents training on your data)",
-                key="not_true_ratings")
-            st.text("")
-            col1, col2 = st.columns([1, 9])
-            with col1:
-                st.form_submit_button("Back", on_click=go_to_mode("rating"))  # TODO save results when going back
-            with col2:
-                st.form_submit_button("Get results!", on_click=go_to_mode("results"))
-
-    if st.session_state["mode"] == "results":
-        slider_dic = {}
-        radio_dic = {}
-        question_dic = {}
-        for key in st.session_state.keys():
-            if key.startswith("permanent_slider"):
-                slider_dic[key[len("permanent_slider_"):]] = st.session_state[key]
-            elif key.startswith("permanent_radio_"):
-                radio_dic[key[len("permanent_radio_"):]] = st.session_state[key]
-            elif key.startswith("permanent_question_"):
-                question_dic[key[len("permanent_question_"):]] = st.session_state[key]
-        question_dic["permanent_favorite_noot"] = st.session_state["permanent_favorite_noot"]
-
-        # pseudo = st.text_input("Pseudo")
-        pseudo = "default"
-
-        def left_align(s, props='text-align: left;'):  # to left align values in dataframe
-            return props
-
-        st.button("Back", on_click=go_to_mode("questions"))
-        st.header("🧠 Your results")
-        st.write("Our model predicted these ratings for you:")
-        st.caption("Some nootropics don't have enough data right now to be included.")
-        st.caption(
-            """ ⚠️"Nootropic" is used here in a broad sense, and some of these substances present a risk of side effects or addiction.""")
-        with st.spinner('Loading...'):
-            new_result_df = predict(slider_dic)
-            new_result_df = new_result_df.sort_values("Prediction", ascending=False, ignore_index=True)
-
-            new_result_df = new_result_df.merge(pd.read_csv("data/nootropics_metadata.csv", sep=";"), on="nootropic",
-                                                how="left")
-            new_result_df["Prediction"] = new_result_df["Prediction"].apply(lambda x: round(x, 1))
-            new_result_df["Mean rating"] = new_result_df["Mean rating"].apply(lambda x: round(x, 1))
-            # new_result_df["Boost"] = new_result_df["Boost"].apply(lambda x: round(x, 1))
-
-            styled_names = []
-            for i, row in new_result_df.iterrows():
-                styled_names.append(make_name(row))
-            new_result_df["Nootropic"] = styled_names
-            new_result_df = new_result_df[["Nootropic", "Prediction", "Mean rating"]]
-            # new_result_df.columns = ["Nootropic",
-            #                         """<div title="The rating we predict you would enter if you tried the nootropic">Prediction</div>""",
-            #                         """<div title="The mean of other users ratings">Mean rating</div>"""]
-            st.write(new_result_df.to_html(escape=False, index=False),
-                     unsafe_allow_html=True)  # .style.format("{:.1f}").applymap(left_align)
-
-        st.write("")
-        st.header("🧠 How accurate is our model?")
-        if True:
-            st.warning("Please rate more nootropics")
-        else:
-            with st.spinner('Loading...'):
-                accuracy_df = evaluate(slider_dic)
-            st.session_state.scroll = False
-            if not accuracy_df is None:
-                # Replace by short results
-                accuracy_df = accuracy_df.merge(
-                    pd.read_csv("data/nootropics_metadata.csv", sep=";")[["nootropic", "nootropic_short"]],
-                    on="nootropic",
-                    how="left")
-                accuracy_df["nootropic"] = accuracy_df["nootropic_short"]
-                accuracy_df = accuracy_df.drop(columns=["nootropic_short"])
-                #
-                st.write("For each nootropics, we hid your rating to our model, and had the model try to guess it.")
-                st.caption("Some nootropics don't have enough data right now to be included.")
-                st.table(accuracy_df.set_index("nootropic").style.format("{:.1f}").applymap(left_align))
-                # print("saving...")
-
-        st.button("Back", on_click=go_to_mode("questions"), key="results_2")
-        st.button("Start again", on_click=go_to_mode("selection"))
         st.write("")
         st.write("")
         st.subheader("🧠 More infos")
